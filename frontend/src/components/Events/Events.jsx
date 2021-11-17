30b722a59d79bf61c60f19c5e13cfc23a3380502
jcovington16 <covingtonjoshua7@gmail.com>
2021-11-16 18:04:32 -0700

    Finished working on the Heroes component. Will have to come back and work on the images. Will now work on creating/registering for events.

--- frontend/src/components/Events/Events.jsx
+++ frontend/src/components/Events/Events.jsx
@@ -7,7 +7,51 @@ const Events = ({user}) => {
             <div>
                 <NavBar user={user}/>
             </div>
-            Events
+            
+            <div className="row">
+
+                <div className="col-md-3"></div>
+
+                <div className="col-md-6">
+                    <h3>Create an Event</h3>
+                    <form action="">
+                        <div className="mb-2">
+                            <label htmlFor="title" className="form-label">Title</label>
+                            <input type="text" className="form-control" placeholder="Event Title"/>
+                        </div>
+
+                        <div className="mb-2">
+                            <label htmlFor="topic" className="form-label">Topic</label>
+                            <input type="text" className="form-control" placeholder="Event Topic"/>
+                        </div>
+
+                        <div className="mb-2">
+                            <label htmlFor="description">Desciption</label>
+                            <textarea name="description" cols="70" rows="10"></textarea>
+                        </div>
+
+                        <div className="mb-2">
+                            <label htmlFor="city" className="form-label">City</label>
+                            <input type="text" className="form-control" placeholder="City of Event" />
+                        </div>
+
+                        <div className="mb-2">
+                            <label htmlFor="address" className="form-label">Address</label>
+                            <input type="text" className="form-control" placeholder="Event Address"/>
+                        </div>
+                        
+                        <div className="mb-2">
+                            <label htmlFor="date" className="form-label">Event Date</label>
+                            <input type="date" className="form-control"/>
+                        </div>
+
+                        <button className="btn-primary">Create Event</button>
+                    </form>
+                </div>
+
+                <div className="col-md-3"></div>
+            </div>
+
         </div>
     )
 }
--- frontend/src/components/Heroes/Heroes.jsx
+++ frontend/src/components/Heroes/Heroes.jsx
@@ -9,12 +9,14 @@ import crypto from 'crypto-js';
 const Heroes = ({user}) => {
 
     const [character, setCharacter] = useState('');
-    const [characterInfo, setCharacterInfo] = useState('');
+    const [characterInfo, setCharacterInfo] = useState();
+    const [characterComics, setCharacterComics] = useState();
 
     const pk = data['public-key']
     const prk = data['private-key']
     const ts = 'thesoer'
     const url = 'https://gateway.marvel.com/v1/public/characters?'
+    const url2 = 'https://gateway.marvel.com/v1/public/characters/'
 
     const hash = crypto.MD5(ts + prk + pk).toString()
 
@@ -26,11 +28,16 @@ const Heroes = ({user}) => {
         e.preventDefault();
         axios.get(`${url}name=${character}&ts=${ts}&apikey=${pk}&hash=${hash}`)
             .then(res => {
-                console.log(res.data)
-                setCharacterInfo(res.data)
+                console.log(res.data.data.results[0])
+                setCharacterInfo(res.data.data.results[0])
+                const id = res.data.data.results[0].id
+                axios.get(`${url2}${id}/comics?limit=15&ts=${ts}&apikey=${pk}&hash=${hash}`)
+                    .then(res => {
+                        console.log(res.data.data.results)
+                        setCharacterComics(res.data.data.results)
+                    })
             })
     }
-
 
 
     return (
@@ -38,19 +45,6 @@ const Heroes = ({user}) => {
             <div>
                 <NavBar user={user}/>
             </div>
-
-            {/* <div className="hero">
-                <div className="hero__search__title">
-                    <span>Search A Hero</span>
-                    <div className="hero__search">
-                        <form onSubmit={handleSearch} className="hero__form">
-                            <input type="text" placeholder="Search" onChange={handleHeroSearch} value={character} />
-                            <button type="submit">Search</button>
-                        </form> 
-                    </div>
-
-                </div>
-            </div> */}
 
             <div className="row mt-5">
                 <div className="col-md-3"></div>
@@ -65,14 +59,55 @@ const Heroes = ({user}) => {
                 <div className="col-md-3"></div>
             </div>
 
-            <div className="row">
-                <div className="col-md-6">
-                
-                </div>
-                <div className="col-md-6">
+            {characterInfo && <div>
+                <div className="row mt-5">
+                    <div className="col-md-6">
+                        <img src={characterInfo.thumbnail} alt="Hero Pic" />
+                    </div>
 
+                    <div className="col-md-6">
+                        <p>{characterInfo.description}</p>
+                    </div>
                 </div>
-            </div>
+                
+                {characterComics &&<div className="row mt-5">
+
+                    <div className="col-md-4">
+                        <div className="card" style={{width: '30rem'}}>
+                            <img src="..." className="card-img-top" alt="Comic Pic"/>
+                            <div className="card-body">
+                                <h5 className="card-title">{characterComics[0].title}</h5>
+                                <p className="card-text">{characterComics[0].description}</p>
+                                <a href={characterComics[0].urls[0].url} className="btn btn-primary">See Comic Book</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-md-4">
+                        <div className="card" style={{width: '30rem'}}>
+                            <img src="..." className="card-img-top" alt="Comic Pic"/>
+                            <div className="card-body">
+                                <h5 className="card-title">{characterComics[1].title}</h5>
+                                <p className="card-text">{characterComics[1].description}</p>
+                                <a href={characterComics[1].urls[0].url} className="btn btn-primary">See Comic Book</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-md-4">
+                        <div className="card" style={{width: '30rem'}}>
+                            <img src="..." className="card-img-top" alt="Comic Pic"/>
+                            <div className="card-body">
+                                <h5 className="card-title">{characterComics[9].title}</h5>
+                                <p className="card-text">{characterComics[9].description}</p>
+                                <a href={characterComics[9].urls[0].url} className="btn btn-primary">See Comic Book</a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>}
+            </div>}
+
 
 
 
